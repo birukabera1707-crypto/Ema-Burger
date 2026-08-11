bdd7df769ccbc9fcc16c11e4f7531445d5eeff4c
biruk <birukabera1707@gmail.com>
2026-08-12 00:48:31 +0300

    Update index.html and database schema

--- models/Sale.js
+++ models/Sale.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const saleSchema = new mongoose.Schema({
+    category: { type: String, required: true },
     itemName: { type: String, required: true },
     quantity: { type: Number, required: true },
     price: { type: Number, required: true },
     total: { type: Number, required: true },
     paymentMethod: { type: String, required: true },
     date: { type: String, required: true }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Sale', saleSchema);
